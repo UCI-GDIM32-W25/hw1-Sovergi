91bd6a1016f5ae7a3bfe2334834a827b7e629129
Wesley Dye <wdye@uci.edu>
2025-01-13 21:24:45 -0800

    Version 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 1.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
-    [SerializeField] private int _numSeeds = 5; 
+    [SerializeField] private int _numSeeds = 5;
     [SerializeField] private PlantCountUI _plantCountUI;
 
     private int _numSeedsLeft;
@@ -13,16 +13,33 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
     }
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector2.up * _speed * Time.deltaTime);
         
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector2.down * _speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector2.left * _speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector2.right * _speed * Time.deltaTime);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (_numSeedsLeft > 0)
+                PlantSeed();
     }
 
     public void PlantSeed ()
     {
-        
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
